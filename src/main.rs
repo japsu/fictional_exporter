20023eab213bfe731a9372d7512aeeecd44b9311
Santtu Pajukanta <santtu@pajukanta.fi>
2020-04-08 21:48:09 +0300

    IT WORKS IT WORKS IT WORKS

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,35 @@ mod graphql;
 mod metrics;
 mod registry;
 
-use juniper::{EmptyMutation, Variables};
+use juniper::EmptyMutation;
+use warp::{http::Response, Filter};
 
 use graphql::{Context, Query, Schema};
 use registry::Registry;
 
-fn main() {
-    let context = Context {
+#[tokio::main]
+async fn main() {
+    ::std::env::set_var("RUST_LOG", "fictional_exporter");
+    env_logger::init();
+
+    let log = warp::log("fictional_exporter");
+
+    // log::info!("Listening on 127.0.0.1:8080");
+
+    let state = warp::any().map(move || Context {
         registry: Registry::dummy(),
-    };
+    });
+    let graphql_filter = juniper_warp::make_graphql_filter(
+        Schema::new(Query, EmptyMutation::<Context>::new()),
+        state.boxed(),
+    );
 
-    let (res, _errors) = juniper::execute(
-        "query { allMetrics { name, labels { key, value } } }",
-        None,
-        &Schema::new(Query, EmptyMutation::new()),
-        &Variables::new(),
-        &context,
+    warp::serve(
+        warp::get2()
+            .and(warp::path("graphiql"))
+            .and(juniper_warp::graphiql_filter("/graphql"))
+            .or(warp::path("graphql").and(graphql_filter))
+            .with(log),
     )
-    .unwrap();
-
-    println!("{:?}", res.as_object_value().unwrap())
+    .run(([127, 0, 0, 1], 8080))
 }
